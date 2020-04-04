637675af02088556806cbe3170cc6ee55fb87c92
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-04-04 14:40:35 +0200

    change one_bit_response

--- tasks.py
+++ tasks.py
@@ -63,6 +63,23 @@ params = {
 
 
 class Task:
+    @staticmethod
+    def task_name():
+        raise NotImplementedError
+
+    @staticmethod
+    def one_bit_response(x, epsilon, alpha, delta, priv_dim=-1):
+        if priv_dim == -1:
+            priv_dim = x.size(1)
+        exp = math.exp(epsilon)
+        x_priv = x[:, :priv_dim]
+        p = (x_priv - alpha[:priv_dim]) / delta[:priv_dim]
+        p[torch.isnan(p)] = 0.  # nan happens when alpha = beta, so also data.x = alpha, so the prev fraction must be 0
+        p = p * (exp - 1) / (exp + 1) + 1 / (exp + 1)
+        x_priv = Bernoulli(p).sample()
+        x = torch.cat([x_priv, x[:, priv_dim:]], dim=1)
+        return x
+
     def __init__(self, dataset, model_name, feature, epsilon, priv_dim):
         self.dataset = dataset
         self.model_name = model_name
@@ -74,20 +91,15 @@ class Task:
     def prepare_data(self):
         data = self.dataset[0]
         if self.feature == 'priv':
-            return self.one_bit_response(data, epsilon=self.epsilon, priv_dim=self.priv_dim)
+            data.x = self.one_bit_response(data.x, self.epsilon, data.alpha, data.delta, self.priv_dim)
         elif self.feature == 'locd':
             num_nodes = data.num_nodes
             data.x = None
             data.num_nodes = num_nodes
-            return LocalDegreeProfile()(data)
-        else:
-            return data
+            data = LocalDegreeProfile()(data)
+        return data
 
     def init_model(self, data):
-        raise NotImplementedError
-
-    @staticmethod
-    def task_name():
         raise NotImplementedError
 
     def run(self):
@@ -98,57 +110,6 @@ class Task:
         optimizer = torch.optim.Adam(model.parameters(), **params[self.task_name()][self.model_name]['optim'])
         model.train_model(loader, optimizer, epochs=params[self.task_name()][self.model_name]['epochs'])
         return model.evaluate(loader)
-
-    @staticmethod
-    def one_bit_response(data, epsilon, priv_dim=-1):
-        if priv_dim == -1:
-            priv_dim = data.num_node_features
-        exp = math.exp(epsilon)
-        x = data.x[:, :priv_dim]
-        p = (x - data.alpha[:priv_dim]) / data.delta[:priv_dim]
-        p[torch.isnan(p)] = 0.  # nan happens when alpha = beta, so also data.x = alpha, so the prev fraction must be 0
-        p = p * (exp - 1) / (exp + 1) + 1 / (exp + 1)
-        x = Bernoulli(p).sample()
-        data.x = torch.cat([x, data.x[:, priv_dim:]], dim=1)
-        return data
-
-
-class ErrorEstimation(Task):
-    @staticmethod
-    def task_name():
-        return 'errorest'
-
-    def __init__(self, dataset, model_name, feature, epsilon, priv_dim):
-        assert model_name == 'gcn' and feature == 'priv'
-        super().__init__(dataset, model_name, feature, epsilon, priv_dim)
-        self.device = 'cuda'
-        data = self.dataset[0].to(self.device)
-        delta = data.delta.clone()
-        delta[delta == 0] = 1  # avoid inf and nan
-        self.delta = delta
-        gcnconv = GCNConv().to(self.device)
-        self.gc = gcnconv(data.x, data.edge_index)
-
-    def init_model(self, data):
-        return GConvDP(epsilon=self.epsilon, alpha=data.alpha, delta=data.delta)
-
-    def prepare_data(self):
-        return self.one_bit_response(self.dataset[0], epsilon=self.epsilon, priv_dim=self.priv_dim)
-
-    @torch.no_grad()
-    def run(self):
-        data = self.prepare_data()
-        model = self.init_model(data)
-        gc_hat = model(data.x, data.edge_index)
-        diff = (self.gc - gc_hat) / self.delta
-        error = torch.norm(diff, p=1, dim=1) / diff.shape[1]
-        deg = self.get_degree(data)
-        return error, deg
-
-    @staticmethod
-    def get_degree(data):
-        row, col = data.edge_index
-        return degree(row, data.num_nodes)
 
 
 class NodeClassification(Task):
@@ -204,3 +165,38 @@ class LinkPrediction(Task):
                 **params['linkpred']['node2vec']['params']
             )
         return model
+
+
+class ErrorEstimation(Task):
+    @staticmethod
+    def task_name():
+        return 'errorest'
+
+    def __init__(self, dataset, model_name, feature, epsilon, priv_dim):
+        assert model_name == 'gcn' and feature == 'priv'
+        super().__init__(dataset, model_name, feature, epsilon, priv_dim)
+        self.device = 'cuda'
+        data = self.dataset[0].to(self.device)
+        delta = data.delta.clone()
+        delta[delta == 0] = 1  # avoid inf and nan
+        self.delta = delta
+        gcnconv = GCNConv().to(self.device)
+        self.gc = gcnconv(data.x, data.edge_index)
+
+    def init_model(self, data):
+        return GConvDP(epsilon=self.epsilon, alpha=data.alpha, delta=data.delta)
+
+    @torch.no_grad()
+    def run(self):
+        data = self.prepare_data().to(self.device)
+        model = self.init_model(data).to(self.device)
+        gc_hat = model(data.x, data.edge_index)
+        diff = (self.gc - gc_hat) / self.delta
+        error = torch.norm(diff, p=1, dim=1) / diff.shape[1]
+        deg = self.get_degree(data)
+        return zip(error.cpu().numpy(), deg.cpu().numpy())
+
+    @staticmethod
+    def get_degree(data):
+        row, col = data.edge_index
+        return degree(row, data.num_nodes)
