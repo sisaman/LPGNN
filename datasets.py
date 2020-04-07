422156691922a0421ed34e12ac60c464b34a20e0
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-04-07 19:15:47 +0200

    add train_test_split_edges

--- datasets.py
+++ datasets.py
@@ -1,16 +1,85 @@
 import json
+import math
 import os.path as osp
+from functools import partial
 
-import torch
 import numpy as np
 import scipy.sparse as sp
-
-from functools import partial
+import torch
 from google_drive_downloader import GoogleDriveDownloader as gdd
-from torch_geometric.data import InMemoryDataset, Data, ClusterData, ClusterLoader, DataLoader
+from torch_geometric.data import InMemoryDataset, Data
 from torch_geometric.datasets import Planetoid, Reddit, PPI, SNAPDataset
 from torch_geometric.transforms import Compose
-from torch_geometric.utils import train_test_split_edges
+from torch_geometric.utils import to_undirected
+
+
+def train_test_split_edges(data, val_ratio=0.05, test_ratio=0.1, rng=None):
+    r"""Splits the edges of a :obj:`torch_geometric.data.Data` object
+    into positive and negative train/val/test edges, and adds attributes of
+    `train_pos_edge_index`, `train_neg_adj_mask`, `val_pos_edge_index`,
+    `val_neg_edge_index`, `test_pos_edge_index`, and `test_neg_edge_index`
+    to :attr:`data`.
+
+    Args:
+        data (Data): The data object.
+        val_ratio (float, optional): The ratio of positive validation
+            edges. (default: :obj:`0.05`)
+        test_ratio (float, optional): The ratio of positive test
+            edges. (default: :obj:`0.1`)
+        rng: random number generator
+
+    :rtype: :class:`torch_geometric.data.Data`
+    """
+
+    assert 'batch' not in data  # No batch-mode.
+
+    row, col = data.edge_index
+    data.edge_index = None
+
+    # Return upper triangular portion.
+    mask = row < col
+    row, col = row[mask], col[mask]
+
+    n_v = int(math.floor(val_ratio * row.size(0)))
+    n_t = int(math.floor(test_ratio * row.size(0)))
+
+    # Positive edges.
+    perm = torch.randperm(row.size(0), generator=rng)
+    row, col = row[perm], col[perm]
+
+    r, c = row[:n_v], col[:n_v]
+    data.val_pos_edge_index = torch.stack([r, c], dim=0)
+    r, c = row[n_v:n_v + n_t], col[n_v:n_v + n_t]
+    data.test_pos_edge_index = torch.stack([r, c], dim=0)
+
+    r, c = row[n_v + n_t:], col[n_v + n_t:]
+    data.train_pos_edge_index = torch.stack([r, c], dim=0)
+    data.train_pos_edge_index = to_undirected(data.train_pos_edge_index)
+
+    # Negative edges.
+    num_nodes = data.num_nodes
+    neg_adj_mask = torch.ones(num_nodes, num_nodes, dtype=torch.uint8)
+    neg_adj_mask = neg_adj_mask.triu(diagonal=1).to(torch.bool)
+    neg_adj_mask[row, col] = 0
+
+    neg_row, neg_col = neg_adj_mask.nonzero().t()
+    perm = torch.randperm(neg_row.size(0), generator=rng, dtype=torch.long)[:min(n_v + n_t, neg_row.size(0))]
+    # perm = random.sample(range(neg_row.size(0)),
+    #                      min(n_v + n_t, neg_row.size(0)))
+    # perm = torch.tensor(perm)
+    # perm = perm.to(torch.long)
+    neg_row, neg_col = neg_row[perm], neg_col[perm]
+
+    neg_adj_mask[neg_row, neg_col] = 0
+    data.train_neg_adj_mask = neg_adj_mask
+
+    row, col = neg_row[:n_v], neg_col[:n_v]
+    data.val_neg_edge_index = torch.stack([row, col], dim=0)
+
+    row, col = neg_row[n_v:n_v + n_t], neg_col[n_v:n_v + n_t]
+    data.test_neg_edge_index = torch.stack([row, col], dim=0)
+
+    return data
 
 
 class Flickr(InMemoryDataset):
@@ -200,12 +269,15 @@ class DataRange:
 
 
 class EdgeSplit:
-    def __init__(self, val_ratio=0.05, test_ratio=0.1):
+    def __init__(self, val_ratio=0.05, test_ratio=0.1, random_state=None):
         self.val_ratio = val_ratio
         self.test_ratio = test_ratio
+        if random_state is not None:
+            self.rng = torch.Generator().manual_seed(random_state)
 
     def __call__(self, data):
-        data = train_test_split_edges(data, self.val_ratio, self.test_ratio)
+        data.train_mask = data.val_mask = data.test_mask = data.y = None
+        data = train_test_split_edges(data, self.val_ratio, self.test_ratio, rng=self.rng)
         data.edge_index = data.train_pos_edge_index
         return data
 
