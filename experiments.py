cf0e8b76e723153d5df903034722e16c85bfe504
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-04-08 18:51:17 +0200

    correct parsed arguments

--- experiments.py
+++ experiments.py
@@ -10,29 +10,14 @@ from argparse import ArgumentParser
 torch.manual_seed(12345)
 
 
+# noinspection PyShadowingNames
 def experiment(args):
     task_class = {
         'nodeclass': NodeClassification,
         'linkpred': LinkPrediction,
         'errorest': ErrorEstimation
     }
-    # datasets = [
-    #     # 'cora',
-    #     # 'citeseer',
-    #     # 'pubmed',
-    #     'flickr'
-    # ]
-    # models = [
-    #     'gcn',
-    #     'node2vec'
-    # ]
-    # features = {
-    #     'gcn': [
-    #         'raw',
-    #         'priv',
-    #         'locd'
-    #     ],
-    # }
+
     epsilons = [0.1, 1, 3, 5, 7, 9]
     epsilons_pr = [1, 3, 5]
     epsilons_err = [0.1, 0.2, 0.5, 1, 2, 5]
@@ -40,28 +25,38 @@ def experiment(args):
 
     for task in args.tasks:
         task = task_class[task]
+
         for dataset_name in args.datasets:
-            dataset = load_dataset(dataset_name, task_name=task.task_name)
-            model_list = ['gcn'] if task is ErrorEstimation else args.models
+            data = load_dataset(dataset_name, task_name=task.task_name)
+            data = data.to('cuda')
+
+            if task is ErrorEstimation: model_list = ['gcn']
+            else: model_list = args.models
+
             for model in model_list:
-                feature_list = ['priv'] if task is ErrorEstimation else args.features
+
+                if task is ErrorEstimation: feature_list = ['priv']
+                elif model == 'node2vec': feature_list = ['void']
+                else: feature_list = args.features
+
                 for feature in feature_list:
                     results = []
-                    pr_list = private_ratios if feature == 'priv' else [0]
+
+                    if feature == 'priv': pr_list = private_ratios
+                    else: pr_list = [0]
+
                     for pr in pr_list:
 
-                        if task is ErrorEstimation:
-                            eps_list = epsilons_err
-                        elif feature == 'priv':
-                            eps_list = epsilons if pr == 1 else epsilons_pr
-                        else:
-                            eps_list = [0]
+                        if task is ErrorEstimation: eps_list = epsilons_err
+                        elif feature != 'priv': eps_list = [0]
+                        elif pr == 1: eps_list = epsilons
+                        else: eps_list = epsilons_pr
 
                         for eps in eps_list:
                             for run in range(1 if task is ErrorEstimation else args.repeats):
                                 t = task(
-                                    dataset=dataset, model_name=model, feature=feature, epsilon=eps,
-                                    priv_dim=int(pr * dataset.num_node_features)
+                                    data=data, model_name=model, feature=feature, epsilon=eps,
+                                    priv_dim=int(pr * data.num_node_features)
                                 )
                                 print(Fore.BLUE + f'\ntask={task.task_name} / dataset={dataset_name} / model={model} / '
                                       f'feature={feature} / pr={pr} / eps={eps} / run={run}' + Style.RESET_ALL)
@@ -78,15 +73,15 @@ def experiment(args):
 
 
 if __name__ == '__main__':
-    tasks = ['nodeclass', 'linkpred', 'errorest']
-    datasets = ['cora', 'citeseer', 'pubmed', 'flickr']
-    models = ['gcn', 'node2vec']
-    features = ['raw', 'priv', 'locd']
+    task_choices = ['nodeclass', 'linkpred', 'errorest']
+    dataset_choices = ['cora', 'citeseer', 'pubmed', 'flickr']
+    model_choices = ['gcn', 'node2vec']
+    feature_choices = ['raw', 'priv', 'locd']
     parser = ArgumentParser()
-    parser.add_argument('-t', '--tasks', nargs='*', choices=tasks, default=tasks)
-    parser.add_argument('-d', '--datasets', nargs='*', choices=datasets, default=datasets)
-    parser.add_argument('-m', '--models', nargs='*', choices=models, default=models)
-    parser.add_argument('-f', '--features', nargs='*', choices=features, default=features)
+    parser.add_argument('-t', '--tasks', nargs='*', choices=task_choices, default=task_choices)
+    parser.add_argument('-d', '--datasets', nargs='*', choices=dataset_choices, default=dataset_choices)
+    parser.add_argument('-m', '--models', nargs='*', choices=model_choices, default=model_choices)
+    parser.add_argument('-f', '--features', nargs='*', choices=feature_choices, default=feature_choices)
     parser.add_argument('-r', '--repeats', type=int, default=10)
     parser.add_argument('-e', '--epochs', type=int, default=500)
 
