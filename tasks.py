76481a42901204f5dd175b4b7f7fb1975147c88f
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-04-14 03:16:56 +0200

    use get_params, unify link prediction and node classification tasks

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import warnings
+from functools import partial
 
 from torch_geometric.transforms import NormalizeFeatures
 
@@ -22,111 +23,13 @@ except ImportError: from sklearn.manifold import TSNE
 
 from gnn import GConvDP
 from models import GCNClassifier, Node2VecClassifier, Node2VecLinkPredictor, VGAELinkPredictor, ResultLogger
-
-params = {
-    'nodeclass': {
-        'gcn': {
-            'params': {
-                'hidden_dim': 16,
-            },
-            'optim': {
-                'cora': {
-                    'lr': 0.01,
-                    'weight_decay': 0.01
-                },
-                'citeseer': {
-                    'lr': 0.01,
-                    'weight_decay': 0.1
-                },
-                'pubmed': {
-                    'lr': 0.01,
-                    'weight_decay': 0.001
-                },
-                'flickr': {
-                    'lr': 0.001,
-                    'weight_decay': 0.0001
-                },
-                'amazon-photo': {
-                    'lr': 0.001,
-                    'weight_decay': 0.0001
-                },
-                'amazon-computers': {
-                    'lr': 0.001,
-                    'weight_decay': 0.0001
-                },
-            }
-        },
-        'node2vec': {
-            'params': {
-                'embedding_dim': 128,
-                'walk_length': 80,
-                'context_size': 10,
-                'walks_per_node': 10,
-                'batch_size': 1
-            },
-        }
-    },
-    'linkpred': {
-        'vgae': {
-            'params': {
-                'output_dim': 16,
-            },
-            'optim': {
-                'cora': {
-                    'lr': 0.01,
-                    'weight_decay': 0.001
-                },
-                'citeseer': {
-                    'lr': 0.01,
-                    'weight_decay': 0.01
-                },
-                'pubmed': {
-                    'lr': 0.1,
-                    'weight_decay': 0.0001
-                },
-                'flickr': {
-                    'lr': 0.001,
-                    'weight_decay': 0.0001
-                },
-                'amazon-photo': {
-                    'lr': 0.001,
-                    'weight_decay': 0.0001
-                },
-                'amazon-computers': {
-                    'lr': 0.001,
-                    'weight_decay': 0.0001
-                },
-            }
-        },
-        'node2vec': {
-            'params': {
-                'embedding_dim': 128,
-                'walk_length': 80,
-                'context_size': 10,
-                'walks_per_node': 10,
-                'batch_size': 1
-            },
-        },
-    }
-}
-
-
-@contextmanager
-def silence_stdout():
-    new_target = open(os.devnull, "w")
-    old_target = sys.stdout
-    sys.stdout = new_target
-    try:
-        yield new_target
-    finally:
-        sys.stdout = old_target
+from params import get_params
 
 
 class Task:
-    @staticmethod
-    def task_name(): raise NotImplementedError
+    task_name = None
 
-    def __init__(self, data, model_name, epsilon=1, **kwargs):
+    def __init__(self, data, model_name, epsilon=1):
         self.model_name = model_name
         self.epsilon = epsilon
         self.data = data
@@ -136,66 +39,50 @@ class Task:
 
 
 class LearningTask(Task):
-    def __init__(self, data, model_name, epsilon=1, **kwargs):
-        super().__init__(data, model_name, epsilon, **kwargs)
-        self.trained_model = None
+    def __init__(self, task_name, data, model_name, epsilon=1):
+        super().__init__(data, model_name, epsilon)
+        self.task_name = task_name
+        self.model = self.get_model(epsilon)
 
-    @abstractmethod
-    def get_model(self): pass
-
-    def run(self, **train_args):
-        logger = ResultLogger()
-        model = self.get_model()
-        early_stop_callback = None if self.model_name == 'node2vec' else EarlyStopping(
-            monitor='val_loss', min_delta=0, patience=10
+    def get_model(self, epsilon):
+        Model = {
+            ('nodeclass', 'gcn'): partial(GCNClassifier, epsilon=epsilon),
+            ('nodeclass', 'node2vec'): Node2VecClassifier,
+            ('linkpred', 'gcn'): partial(VGAELinkPredictor, epsilon=epsilon),
+            ('linkpred', 'node2vec'): Node2VecLinkPredictor,
+        }
+        return Model[self.task_name, self.model_name](
+            data=self.data,
+            **get_params(
+                section='model', task=self.task_name, dataset=self.data.name, model=self.model_name
+            )
         )
+
+    def run(self):
+        logger = ResultLogger()
+        early_stop_callback = None if self.model_name == 'node2vec' else EarlyStopping(**get_params(
+            section='early-stop', task=self.task_name, dataset=self.data.name, model=self.model_name
+        ))
         # noinspection PyTypeChecker
         trainer = Trainer(
             gpus=1, checkpoint_callback=False, logger=logger, weights_summary=None,
-            early_stop_callback=early_stop_callback, **train_args
+            early_stop_callback=early_stop_callback, **get_params(
+                section='trainer', task=self.task_name, dataset=self.data.name, model=self.model_name
+            )
         )
-        trainer.fit(model)
-        with silence_stdout(): trainer.test()
-        self.trained_model = model
+        trainer.fit(self.model)
+        with self.silence_stdout(): trainer.test()
         return logger.result
 
-
-class NodeClassification(LearningTask):
-    task_name = 'nodeclass'
-
-    def get_model(self):
-        if self.model_name == 'gcn':
-            # noinspection PyArgumentList
-            return GCNClassifier(
-                data=self.data,
-                epsilon=self.epsilon,
-                **params[self.task_name][self.model_name]['params'],
-                **params[self.task_name][self.model_name]['optim'][self.data.name],
-            )
-        elif self.model_name == 'node2vec':
-            return Node2VecClassifier(
-                data=self.data,
-                **params[self.task_name][self.model_name]['params']
-            )
-
-
-class LinkPrediction(LearningTask):
-    task_name = 'linkpred'
-
-    def get_model(self):
-        if self.model_name == 'vgae':
-            # noinspection PyArgumentList
-            return VGAELinkPredictor(
-                data=self.data,
-                epsilon=self.epsilon,
-                **params[self.task_name][self.model_name]['params'],
-                **params[self.task_name][self.model_name]['optim'][self.data.name],
-            )
-        elif self.model_name == 'node2vec':
-            return Node2VecLinkPredictor(
-                data=self.data,
-                **params[self.task_name][self.model_name]['params']
-            )
+    @contextmanager
+    def silence_stdout(self):
+        new_target = open(os.devnull, "w")
+        old_target = sys.stdout
+        sys.stdout = new_target
+        try:
+            yield new_target
+        finally:
+            sys.stdout = old_target
 
 
 class ErrorEstimation(Task):
@@ -221,30 +108,23 @@ class ErrorEstimation(Task):
         return degree(row, data.num_nodes)
 
 
-class Visualization(LinkPrediction):
-    task_name = 'visualize'
+class Visualization(LearningTask):
+    def __init__(self, data, epsilon=1):
+        super().__init__(task_name='linkpred', data=data, model_name='gcn', epsilon=epsilon)
 
-    def get_model(self):
-        # noinspection PyArgumentList
-        return VGAELinkPredictor(
-            data=self.data,
-            epsilon=self.epsilon,
-            **params['linkpred'][self.model_name]['params'],
-            **params['linkpred'][self.model_name]['optim'][self.data.name],
-        )
-
-    def run(self, **train_args):
-        super().run(**train_args)
-        z = self.trained_model(self.data)
+    def run(self):
+        super().run()
+        z = self.model(self.data)
         embedding = TSNE(n_components=2).fit_transform(z.cpu().detach().numpy())
         label = self.data.y.cpu().numpy()
         return {'data': embedding, 'label': label}
 
 
-if __name__ == '__main__':
+def main():
     dataset = load_dataset('cora', split_edges=True).to('cuda')
-    eps = 5
-    dataset = privatize(dataset, pnr=1, pfr=1, eps=eps, method='lap')
-    dataset = NormalizeFeatures()(dataset)
-    task = NodeClassification(dataset, 'gcn', epsilon=eps)
-    print(task.run(min_epochs=10, max_epochs=500))
+    task = LearningTask(task_name='linkpred', data=dataset, model_name='node2vec', epsilon=1)
+    print(task.run())
+
+
+if __name__ == '__main__':
+    main()
