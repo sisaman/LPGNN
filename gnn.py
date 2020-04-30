b8cd006190e85671ef3360eecea96a33a74bc7f4
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-04-30 19:10:24 +0200

    replace GConvDP with GConv

--- gnn.py
+++ gnn.py
@@ -1,4 +1,3 @@
-import math
 import torch
 import torch.nn.functional as F
 from torch.nn import Linear, BatchNorm1d
@@ -6,50 +5,40 @@ from torch_geometric.nn import MessagePassing, GCNConv
 from torch_geometric.utils import add_remaining_self_loops, degree
 
 
-class GConvDP(MessagePassing):
-    @staticmethod
-    def norm(edge_index, num_nodes, dtype):
-        edge_index, _ = add_remaining_self_loops(edge_index, num_nodes=num_nodes)
-        row, col = edge_index
-        deg = degree(row, num_nodes, dtype=dtype)
-        deg_inv_sqrt = deg.pow(-0.5)
-        return edge_index, (deg_inv_sqrt[row] * deg_inv_sqrt[col]).view(-1, 1)
-
-    def __init__(self, epsilon=1, alpha=0, delta=0, cached=False):
+class GConv(MessagePassing):
+    def __init__(self, cached=False):
         super().__init__(aggr='add')  # "Add" aggregation.
-        self.eps = epsilon
-        self.alpha = alpha
-        self.delta = delta
         self.cached = cached
         self.cached_gc = None
 
-    def forward(self, x, edge_index, priv_mask):
+    def forward(self, x, edge_index):
         if not self.cached or self.cached_gc is None:
             num_nodes = x.size(0)
-            edge_index, norm = self.norm(edge_index, num_nodes, x.dtype)
-            self.cached_gc = self.propagate(edge_index, x=x, norm=norm, p=priv_mask)
+            edge_index, _ = add_remaining_self_loops(edge_index, num_nodes=num_nodes)
+            row, col = edge_index
+            deg = degree(row, num_nodes, dtype=x.dtype)
+            deg_inv_sqrt = deg.pow(-0.5)
+            norm = (deg_inv_sqrt[row] * deg_inv_sqrt[col]).view(-1, 1)
+            self.cached_gc = self.propagate(edge_index, x=x, norm=norm)
         return self.cached_gc
 
     # noinspection PyMethodOverriding
-    def message(self, x_j, p_j, norm):
-        exp = math.exp(self.eps)
-        msg = norm * (p_j * ((((exp + 1) * x_j - 1) * self.delta) / (exp - 1) + self.alpha) + (~p_j) * x_j)
-        return msg
+    def message(self, x_j, norm):
+        return norm * x_j
 
 
 class GCN(torch.nn.Module):
-    def __init__(self, input_dim, output_dim, hidden_dim, dropout=0.5, epsilon=1, alpha=0, delta=0, cached=True):
-        assert epsilon > 0
+    def __init__(self, input_dim, output_dim, hidden_dim, dropout=0.5, cached=True):
         super().__init__()
-        self.conv1 = GConvDP(epsilon, alpha, delta, cached=cached)
+        self.conv1 = GConv(cached=cached)
         self.lin1 = Linear(input_dim, hidden_dim)
         self.conv2 = GCNConv(hidden_dim, output_dim, cached=cached)
         self.dropout = dropout
         self.cached = cached
         self.cached_gc = None
 
-    def forward(self, x, edge_index, priv_mask):
-        x = self.conv1(x, edge_index, priv_mask)
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
         x = self.lin1(x)
         x = torch.relu(x)
         x = F.dropout(x, p=self.dropout, training=self.training)
@@ -58,16 +47,16 @@ class GCN(torch.nn.Module):
 
 
 class GraphEncoder(torch.nn.Module):
-    def __init__(self, input_dim, output_dim, epsilon=1, alpha=0, delta=0, cached=True):
+    def __init__(self, input_dim, output_dim, cached=True):
         super().__init__()
-        self.conv1 = GConvDP(epsilon, alpha, delta, cached=cached)
+        self.conv1 = GConv(cached=cached)
         self.lin1 = Linear(input_dim, 2 * output_dim)
         self.bn = BatchNorm1d(2 * output_dim)
         self.conv_mu = GCNConv(2 * output_dim, output_dim, cached=cached)
         self.conv_logvar = GCNConv(2 * output_dim, output_dim, cached=cached)
 
-    def forward(self, x, edge_index, priv_mask):
-        x = self.conv1(x, edge_index, priv_mask)
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
         x = self.lin1(x)
         x = self.bn(x)
         x = F.relu(x)
