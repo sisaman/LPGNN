99fa19cca4271a692da03e1cbda33beaf9d90b17
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-05-22 22:24:58 +0200

    use mlflow logger

--- experiments.py
+++ experiments.py
@@ -1,122 +1,115 @@
-import warnings
-
-warnings.simplefilter(action='ignore', category=FutureWarning)
-warnings.simplefilter(action='ignore', category=RuntimeWarning)
-
-import os
+import time
+from loggers import CustomMLFlowLogger
 from argparse import ArgumentParser
-
-import pandas as pd
 from colorama import Fore, Style
 from datasets import load_dataset, get_available_datasets
 from mechanisms import privatize, available_mechanisms
 from tasks import LearningTask, ErrorEstimation, Task
 from pytorch_lightning import seed_everything
 
-seed_everything(12345)
 
-private_node_ratios = [.2, .4, .6, .8, 1]
-private_feature_ratios = [.2, .4, .6, .8, 1]
-epsilons = [1, 3, 5, 7, 9]
-epsilons_priv_ratio = [1, 3, 5]
-
-
-def get_pnr_pfr_lists(feature, pnr_list, pfr_list):
+def get_ratios(feature, rfr, pfr_list):
+    result = {(0, 1)}
     if feature == 'bit':
-        return {(pnr, 1) for pnr in pnr_list} | {(1, pfr) for pfr in pfr_list}
-    elif feature in available_mechanisms:
-        return [(1, 1)]
+        result |= {(1-pfr, pfr) for pfr in pfr_list} | {(rfr, pfr) for pfr in pfr_list}
+    return sorted(list(result))
+
+
+def get_eps_list(feature, pfr):
+    if feature == 'raw':
+        return [None]
+    if feature == 'bit' and pfr < 1:
+        return set(epsilons_priv_ratio) & set(args.epsilons)
     else:
-        return [(0, 0)]
+        return args.epsilons
 
 
-def get_eps_list(feature, pnr, pfr):
-    if feature == 'bit':
-        if pnr == 1 and pfr == 1:
-            return epsilons
-        else:
-            return epsilons_priv_ratio
-    elif feature in available_mechanisms:
-        return epsilons
-    else:
-        return [1]
-
-
-def save_results(task_name, dataset_name, model_name, feature, results, output):
-    df_result = pd.DataFrame(
-        data=results,
-        columns=['method', 'pnr', 'pfr', 'eps', 'run', 'perf']
-    )
-
-    path = os.path.join(output, f'{task_name}_{dataset_name}_{model_name}_{feature}.pkl')
-    df_result.to_pickle(path)
-
-
-def error_estimation(args):
+def error_estimation():
     for dataset_name in args.datasets:
         dataset = load_dataset(dataset_name).to('cuda')
-        for feature in available_mechanisms & set(args.features):
-            results = []
-            for pnr, pfr in get_pnr_pfr_lists(feature, args.pnr_list, args.pfr_list):
-                for eps in epsilons:
-                    for run in range(args.repeats):
-                        print(
-                            Fore.BLUE +
-                            f'\ntask=error / dataset={dataset_name} / model=gcn / '
-                            f'feature={feature} / pnr={pnr} / pfr={pfr} / eps={eps} / run={run}'
-                            + Style.RESET_ALL
-                        )
+        for model in args.models:
+            for feature in available_mechanisms & set(args.features):
+                for pfr in set(args.pfr_list) | {1}:
+                    for eps in get_eps_list(feature, pfr):
+                        for run in range(args.repeats):
+                            print(
+                                Fore.BLUE +
+                                f'\ntask=error / dataset={dataset_name} / model={model} / '
+                                f'feature={feature} / pfr={pfr} / eps={eps} / run={run}'
+                                + Style.RESET_ALL
+                            )
 
-                        data = privatize(dataset, pnr=pnr, pfr=pfr, eps=eps, method=feature)
-                        t = ErrorEstimation(data=data, orig_features=dataset.x)
-                        result = t.run()
-                        results.append((f'gcn+{feature}', pnr, pfr, eps, run, result))
+                            data = privatize(dataset, method=feature, rfr=1-pfr, pfr=pfr, eps=eps)
+                            t = ErrorEstimation(data=data, orig_features=dataset.x)
+                            result = t.run()
+                            # results.append((f'gcn+{feature}', pnr, pfr, 1, eps, run, result))
 
-            save_results('error', dataset_name, 'gcn', feature, results, args.output)
+            # save_results('error', dataset_name, 'gcn', feature, results, args.output)
 
 
-def prediction(task, args):
+def prediction(task):
+    # init experiment logger
+    logger = CustomMLFlowLogger(experiment_name='dpgcn-prediction')
+
     for dataset_name in args.datasets:
         dataset = load_dataset(dataset_name, split_edges=(task == 'link'))
         dataset = dataset.to('cuda')
 
         for model in args.models:
             for feature in args.features:
-                results = []
-                pr_list = get_pnr_pfr_lists(feature, args.pnr_list, args.pfr_list)
+                for rfr, pfr in get_ratios(feature, args.raw_feature_ratio, args.pfr_list):
+                    for eps in get_eps_list(feature, pfr):
+                        params = {
+                            'task': task,
+                            'dataset': dataset_name,
+                            'model': model,
+                            'feature': feature,
+                            'rfr': rfr,
+                            'pfr': pfr,
+                            'eps': eps,
+                        }
 
-                for pnr, pfr in pr_list:
-                    for eps in get_eps_list(feature, pnr, pfr):
+                        params_str = ' | '.join([f'{key}={val}' for key, val in params.items()])
+                        logger.delete_runs(filter_string=f"tags.params='{params_str}'")
+
                         for run in range(args.repeats):
-                            print(
-                                Fore.BLUE +
-                                f'\ntask={task} / dataset={dataset_name} / model={model} / '
-                                f'feature={feature} / pnr={pnr} / pfr={pfr} / eps={eps} / run={run}'
-                                + Style.RESET_ALL
-                            )
+                            params['run'] = run
 
-                            data = privatize(dataset, pnr=pnr, pfr=pfr, eps=eps, method=feature)
+                            print(Fore.BLUE + params_str + f' | run={run}' + Style.RESET_ALL)
+
+                            logger.create_run(tags={'params': params_str})
+                            logger.log_params(params)
+
+                            data = dataset
+                            if feature in available_mechanisms:
+                                data = privatize(data, method=feature, pfr=pfr, rfr=rfr, eps=eps)
+
                             t = LearningTask(task_name=task, data=data, model_name=model)
-                            result = t.run()
-                            print(result)
-                            results.append((f'{model}+{feature}', pnr, pfr, eps, run, result))
-
-                save_results(task, dataset_name, model, feature, results, args.output)
+                            t.run(logger)
 
 
-def main(args):
+def main():
     for task in args.tasks:
         if task in LearningTask.task_list():
-            prediction(task, args)
+            prediction(task)
         elif task == Task.ErrorEstimation:
-            error_estimation(args)
+            error_estimation()
 
 
 if __name__ == '__main__':
+    seed_everything(12345)
+
+    # default values
+    raw_feature_ratio = 0.2
+    private_feature_ratios = [.2, .4, .6, .8]
+    epsilons = [1, 3, 5, 7, 9]
+    epsilons_priv_ratio = [3, 5, 7]
     task_choices = Task.task_list()
     dataset_choices = get_available_datasets()
     model_choices = ['gcn']
     feature_choices = ['raw'] + list(available_mechanisms)
+
+    # parse arguments
     parser = ArgumentParser()
     parser.add_argument('-t', '--tasks', nargs='*', choices=task_choices, default=task_choices)
     parser.add_argument('-d', '--datasets', nargs='*', choices=dataset_choices, default=dataset_choices)
@@ -124,12 +117,13 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--features', nargs='*', choices=feature_choices, default=feature_choices)
     parser.add_argument('-r', '--repeats', type=int, default=10)
     parser.add_argument('-o', '--output', type=str, default='results')
-    parser.add_argument('--pnr', nargs='*', type=float, default=private_node_ratios, dest='pnr_list')
     parser.add_argument('--pfr', nargs='*', type=float, default=private_feature_ratios, dest='pfr_list')
+    parser.add_argument('--rfr', type=float, default=raw_feature_ratio, dest='raw_feature_ratio')
     parser.add_argument('--eps', nargs='*', type=float, default=epsilons, dest='epsilons')
+    args = parser.parse_args()
+    print(args)
 
-    arguments = parser.parse_args()
-    print(arguments)
-    epsilons = arguments.epsilons
-
-    main(arguments)
+    start = time.time()
+    main()
+    end = time.time()
+    print('Done in', end - start, 'seconds.')
