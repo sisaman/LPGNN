0662d058705e01b89daa2c62db0b3feddc29eb18
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-05-23 11:42:13 +0200

    remove MLFlow logger, add Pandas logger

--- loggers.py
+++ loggers.py
@@ -1,45 +1,71 @@
-from pytorch_lightning.loggers import MLFlowLogger
-from pytorch_lightning import _logger as log
+import os
+import pandas as pd
+from pytorch_lightning.loggers import LightningLoggerBase
 
 
-class CustomMLFlowLogger(MLFlowLogger):
-    def __init__(self, experiment_name):
-        super().__init__(experiment_name=experiment_name)
-        self._expt_id = None
+class PandasLogger(LightningLoggerBase):
+
+    def __init__(self, experiment_name, save_dir):
+        super().__init__()
+        self.experiment_name = experiment_name
+        self.df_dir = os.path.join(save_dir, self.experiment_name + '.pkl')
+        self.data = []
+        self.metrics = {}
+        self.params = None
+        self.df = None
 
     def log_metrics(self, metrics, step=None):
         if 'test_result' in metrics:
-            super().log_metrics(metrics, step)
+            self.metrics.update(metrics)
+        pass
 
     def log_params(self, params):
-        for key, value in params.items():
-            self.experiment.log_param(self.run_id, key, value)
+        self._add_run_to_data()
+        self.params = params
+        pass
 
-    def create_run(self, tags=None):
-        run = self._mlflow_client.create_run(experiment_id=self.experiment_id, tags=tags)
-        self._run_id = run.info.run_id
-        return self._run_id
+    def log_hyperparams(self, params):
+        pass
 
-    def delete_runs(self, filter_string):
-        query_results = self.experiment.search_runs(experiment_ids=self.experiment_id, filter_string=filter_string)
-        for result in query_results:
-            self.experiment.delete_run(result.info.run_id)
+    def dump(self):
+        print('\n\nDUMP\n\n')
+        params = self.params.keys()
+        self.df = self._open_or_create_dataframe()
+        self._add_run_to_data()
+        df_new = pd.DataFrame(self.data)
+        self.data = []
+        self.df = self.df.append(df_new, ignore_index=True)
+        self.df.drop_duplicates(subset=params, keep='last', inplace=True, ignore_index=True)
+        self.df.to_pickle(path=self.df_dir)
+
+    def _add_run_to_data(self):
+        if self.params is not None:
+            run = {**self.params, **self.metrics}
+            self.data.append(run)
+            self.metrics = {}
+            self.params = None
+
+    def _open_or_create_dataframe(self):
+        try:
+            self.df = pd.read_pickle(self.df_dir)
+        except FileNotFoundError:
+            self.df = pd.DataFrame()
+        return self.df
 
     @property
-    def experiment_id(self):
-        if self._expt_id is None:
-            expt = self.experiment.get_experiment_by_name(self.experiment_name)
-
-            if expt:
-                self._expt_id = expt.experiment_id
-            else:
-                log.warning(f'Experiment with name {self.experiment_name} not found. Creating it.')
-                self._expt_id = self._mlflow_client.create_experiment(name=self.experiment_name)
-
-        return self._expt_id
+    def experiment(self):
+        return self.data
 
     @property
-    def run_id(self):
-        if self._run_id is None:
-            self._run_id = self.create_run()
-        return self._run_id
+    def name(self):
+        return self.experiment_name
+
+    @property
+    def version(self):
+        return 1
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.dump()
