d850c2bb36f3bfec5fb1fac0a6c266917b7df71a
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-05-24 19:49:51 +0200

    add save_mode

--- loggers.py
+++ loggers.py
@@ -5,10 +5,12 @@ from pytorch_lightning.loggers import LightningLoggerBase
 
 class PandasLogger(LightningLoggerBase):
 
-    def __init__(self, experiment_name, save_dir):
+    def __init__(self, experiment_name, save_dir, save_mode='replace'):
+        assert save_mode in ['replace', 'truncate']
         super().__init__()
         self.experiment_name = experiment_name
         self.df_dir = os.path.join(save_dir, self.experiment_name + '.pkl')
+        self.save_mode = save_mode
         self.data = []
         self.metrics = {}
         self.params = None
@@ -30,12 +32,16 @@ class PandasLogger(LightningLoggerBase):
     def dump(self):
         print('\n\nDUMP\n\n')
         params = self.params.keys()
-        self.df = self._open_or_create_dataframe()
         self._add_run_to_data()
         df_new = pd.DataFrame(self.data)
         self.data = []
+
+        self.df = self._open_or_create_dataframe()
         self.df = self.df.append(df_new, ignore_index=True)
-        self.df.drop_duplicates(subset=params, keep='last', inplace=True, ignore_index=True)
+
+        if self.save_mode == 'replace':
+            self.df.drop_duplicates(subset=params, keep='last', inplace=True, ignore_index=True)
+
         self.df.to_pickle(path=self.df_dir)
 
     def _add_run_to_data(self):
@@ -46,10 +52,12 @@ class PandasLogger(LightningLoggerBase):
             self.params = None
 
     def _open_or_create_dataframe(self):
-        try:
-            self.df = pd.read_pickle(self.df_dir)
-        except FileNotFoundError:
-            self.df = pd.DataFrame()
+        self.df = pd.DataFrame()
+        if self.save_mode == 'replace':
+            try:
+                self.df = pd.read_pickle(self.df_dir)
+            except FileNotFoundError:
+                pass
         return self.df
 
     @property
