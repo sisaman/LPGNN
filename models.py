d87b6b3f6081eb17272f93726e592ddeedf9c1d9
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-06-19 14:21:10 +0200

    move code from gnn.py -- parameterize with hparams

--- models.py
+++ models.py
@@ -1,49 +1,75 @@
-import logging
+from argparse import ArgumentParser
 
 import torch
 import torch.nn.functional as F
-from pytorch_lightning import Trainer, LightningModule, seed_everything
-from pytorch_lightning.callbacks import EarlyStopping
+from pytorch_lightning import LightningModule
+from torch.nn import BatchNorm1d
 from torch.optim import Adam
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch_geometric.nn import GCNConv
 from torch_geometric.nn import VGAE
 
-from datasets import load_dataset, GraphLoader
-from gnn import GCN, GraphEncoder
-from privacy import privatize
 
-logging.disable(logging.INFO)
+class GCN(torch.nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim, dropout, inductive=False):
+        super().__init__()
+        self.conv1 = GCNConv(input_dim, hidden_dim, cached=not inductive)
+        self.conv2 = GCNConv(hidden_dim, output_dim, cached=not inductive)
+        self.dropout = dropout
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = torch.relu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.conv2(x, edge_index)
+        x = F.log_softmax(x, dim=1)
+        return x
+
+
+class GraphEncoder(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, inductive=False):
+        super().__init__()
+        self.conv = GCNConv(input_dim, hidden_dim, cached=not inductive)
+        self.bn = BatchNorm1d(hidden_dim)
+        self.conv_mu = GCNConv(hidden_dim, output_dim, cached=not inductive)
+        self.conv_logvar = GCNConv(hidden_dim, output_dim, cached=not inductive)
+
+    def forward(self, x, edge_index):
+        x = self.conv(x, edge_index)
+        x = self.bn(x)
+        x = F.relu(x)
+        return self.conv_mu(x, edge_index), self.conv_logvar(x, edge_index)
 
 
 class NodeClassifier(LightningModule):
-    def __init__(self, data, hidden_dim=16, dropout=0.5, lr=0.01, weight_decay=5e-4):
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument('--hidden-dim', type=int, default=16)
+        parser.add_argument('--dropout', type=float, default=0.5)
+        parser.add_argument('--learning-rate', type=float, default=0.01)
+        parser.add_argument('--weight-decay', type=float, default=5e-4)
+        parser.add_argument('--min-epochs', type=int, default=10)
+        parser.add_argument('--max-epochs', type=int, default=500)
+        parser.add_argument('--min-delta', type=float, default=0.0)
+        parser.add_argument('--patience', type=int, default=20)
+        return parser
+
+    def __init__(self, num_classes, input_dim, hparams):
         super().__init__()
-        self.data = data
-        self.lr = lr
-        self.weight_decay = weight_decay
-        self.gnn = GCN(
-            input_dim=data.num_node_features,
-            output_dim=data.num_classes,
-            hidden_dim=hidden_dim,
-            dropout=dropout,
+        self.hparams = hparams
+        self.model = GCN(
+            input_dim=input_dim,
+            output_dim=num_classes,
+            hidden_dim=hparams.hidden_dim,
+            dropout=hparams.dropout,
+            inductive=False
         )
 
     def forward(self, data):
-        return self.gnn(data.x, data.edge_index)
+        return self.model(data.x, data.edge_index)
 
     def configure_optimizers(self):
-        optimizer = Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
-        scheduler = ReduceLROnPlateau(optimizer)
-        return [optimizer], [scheduler]
-
-    def train_dataloader(self):
-        return GraphLoader(self.data)
-
-    def val_dataloader(self):
-        return GraphLoader(self.data)
-
-    def test_dataloader(self):
-        return GraphLoader(self.data)
+        return Adam(self.parameters(), lr=self.hparams.learning_rate, weight_decay=self.hparams.weight_decay)
 
     def training_step(self, data, index):
         out = self(data)
@@ -76,34 +102,39 @@ class NodeClassifier(LightningModule):
 
 
 class LinkPredictor(LightningModule):
-    def __init__(self, data, output_dim=16, lr=0.01, weight_decay=0.0):
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument('--encoder-hidden-dim', type=int, default=32)
+        parser.add_argument('--encoder-output-dim', type=int, default=16)
+        parser.add_argument('--learning-rate', type=float, default=0.01)
+        parser.add_argument('--weight-decay', type=float, default=0.0)
+        parser.add_argument('--min-epochs', type=int, default=100)
+        parser.add_argument('--max-epochs', type=int, default=500)
+        parser.add_argument('--check-val-every-n-epoch', type=int, default=10)
+        parser.add_argument('--min-delta', type=float, default=0.0)
+        parser.add_argument('--patience', type=int, default=10)
+        return parser
+
+    def __init__(self, input_dim, hparams):
         super().__init__()
-        self.data = data
-        self.lr = lr
-        self.weight_decay = weight_decay
+        self.hparams = hparams
 
         encoder = GraphEncoder(
-            input_dim=data.num_node_features,
-            output_dim=output_dim
+            input_dim=input_dim,
+            hidden_dim=hparams.encoder_hidden_dim,
+            output_dim=hparams.encoder_output_dim
         )
 
         self.model = VGAE(encoder=encoder)
+        self.trainer = None
 
     def forward(self, data):
         x = self.model.encode(data.x, data.train_pos_edge_index)
         return x
 
-    def train_dataloader(self):
-        return GraphLoader(self.data)
-
-    def val_dataloader(self):
-        return GraphLoader(self.data)
-
-    def test_dataloader(self):
-        return GraphLoader(self.data)
-
     def configure_optimizers(self):
-        return Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        return Adam(self.parameters(), lr=self.hparams.learning_rate, weight_decay=self.hparams.weight_decay)
 
     def model_loss(self, data, pos_edge_index):
         z = self(data)
@@ -132,42 +163,3 @@ class LinkPredictor(LightningModule):
         auc = torch.tensor([x['auc'] for x in outputs]).mean().item()
         log = {'test_result': auc}
         return {'test_auc': auc, 'log': log}
-
-
-def main():
-    seed_everything(12345)
-
-    data = load_dataset(
-        dataset_name='cora',
-        # split_edges=True
-    ).to('cuda')
-
-    data = privatize(data, pfr=1, eps=100, method='pws-m')
-
-    for i in range(1):
-        print('RUN', i)
-        model = NodeClassifier(data, lr=.01, weight_decay=0.01, dropout=0.5)
-        # model = GraphSAGEClassifier(data, lr=.01, weight_decay=0.01, dropout=0.5)
-        # model = VGAELinkPredictor(data, lr=0.01, weight_decay=0.001)
-
-        # noinspection PyTypeChecker
-        trainer = Trainer(
-            gpus=1, max_epochs=500, checkpoint_callback=False,
-            early_stop_callback=EarlyStopping(monitor='val_loss', min_delta=0, patience=20),
-            # early_stop_callback=False,
-            weights_summary=None,
-            # min_epochs=100,
-            logger=False,
-            # check_val_every_n_epoch=10
-        )
-
-        # lr_finder = trainer.lr_find(model)
-        # print(lr_finder.suggestion())
-        # fig = lr_finder.plot(suggest=True)
-        # fig.show()
-        trainer.fit(model)
-        trainer.test()
-
-
-if __name__ == '__main__':
-    main()
