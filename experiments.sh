a8f1223b9151bc5d6242cbc17728f585732d73fc
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-08-20 22:56:08 +0200

    Update experiments.sh

--- experiments.sh
+++ experiments.sh
@@ -10,16 +10,15 @@ python train.py -t node -d citeseer -m lm pm pgc  -e 1 5 9  -r 10 --learning-rat
 ### Cora
 python train.py -t node -d cora     -m raw        -e 0      -r 10 --learning-rate 0.01  --weight-decay 0.01     --dropout 0.5
 python train.py -t node -d cora     -m lm pm pgc  -e 1 5 9  -r 10 --learning-rate 0.01  --weight-decay 0.01     --dropout 0.5
+### Twitch
+python train.py -t node -d twitch   -m raw        -e 0      -r 10 --learning-rate 0.001 --weight-decay 0.0001   --dropout 0
+python train.py -t node -d twitch   -m lm pm pgc  -e 1 5 9  -r 10 --learning-rate 0.001 --weight-decay 0.0001   --dropout 0
 ### Elliptic
 python train.py -t node -d elliptic -m raw        -e 0      -r 10 --learning-rate 0.01  --weight-decay 0        --dropout 0
 python train.py -t node -d elliptic -m lm pm pgc  -e 1 5 9  -r 10 --learning-rate 0.01  --weight-decay 0        --dropout 0
 ### Flickr
 python train.py -t node -d flickr   -m raw        -e 0      -r 10 --learning-rate 0.01  --weight-decay 0        --dropout 0
 python train.py -t node -d flickr   -m lm pm pgc  -e 1 5 9  -r 10 --learning-rate 0.01  --weight-decay 0        --dropout 0
-### Twitch
-python train.py -t node -d twitch   -m raw        -e 0      -r 10 --learning-rate 0.001 --weight-decay 0.0001   --dropout 0
-python train.py -t node -d twitch   -m lm pm pgc  -e 1 5 9  -r 10 --learning-rate 0.001 --weight-decay 0.0001   --dropout 0
-
 
 
 # Link Prediction
@@ -29,12 +28,12 @@ python train.py -t link -d citeseer -m lm pm pgc  -e 1 5 9  -r 10 --learning-rat
 ### Cora
 python train.py -t link -d cora     -m raw        -e 0      -r 10 --learning-rate 0.01  --weight-decay 0.01
 python train.py -t link -d cora     -m lm pm pgc  -e 1 5 9  -r 10 --learning-rate 0.01  --weight-decay 0.01
+### Twitch
+python train.py -t link -d twitch   -m raw        -e 0      -r 10 --learning-rate 0.01  --weight-decay 0.001
+python train.py -t link -d twitch   -m lm pm pgc  -e 1 5 9  -r 10 --learning-rate 0.01  --weight-decay 0.001
 ### Elliptic
 python train.py -t link -d elliptic -m raw        -e 0      -r 10 --learning-rate 0.01  --weight-decay 0.0001
 python train.py -t link -d elliptic -m lm pm pgc  -e 1 5 9  -r 10 --learning-rate 0.01  --weight-decay 0.0001
 ### Flickr
 python train.py -t link -d flickr   -m raw        -e 0      -r 10 --learning-rate 0.01  --weight-decay 0.001
 python train.py -t link -d flickr   -m lm pm pgc  -e 1 5 9  -r 10 --learning-rate 0.01  --weight-decay 0.001
-### Twitch
-python train.py -t link -d twitch   -m raw        -e 0      -r 10 --learning-rate 0.01  --weight-decay 0.001
-python train.py -t link -d twitch   -m lm pm pgc  -e 1 5 9  -r 10 --learning-rate 0.01  --weight-decay 0.001
