b292dd4d1c7b47d85f18e745f8997c7221277bec
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-09-08 00:04:48 +0200

    Update error.py

--- error.py
+++ error.py
@@ -1,3 +1,4 @@
+import os
 from argparse import ArgumentParser
 
 import torch
@@ -6,45 +7,41 @@ from pytorch_lightning.loggers import TensorBoardLogger
 from torch_geometric.nn import GCNConv
 from torch_geometric.utils import degree
 import pandas as pd
-from datasets import get_available_datasets, GraphDataset
+from datasets import get_available_datasets, GraphDataModule
+from transforms import Privatize
 from utils import TermColors
-from privacy import get_available_mechanisms, Privatize
+from privacy import get_available_mechanisms
 
 
 class ErrorEstimation:
-    def __init__(self, data, max_degree_quantile=0.99, device='cuda'):
+    def __init__(self, data, logger, max_degree_quantile=0.99, device='cuda'):
         self.data = data
         alpha = data.x_raw.min(dim=0)[0]
         beta = data.x_raw.max(dim=0)[0]
-        self.delta = beta - alpha
+        self.sensitivity = beta - alpha
+        self.logger = logger
         self.max_degree_quantile = max_degree_quantile
+        device = 'cpu' if not torch.cuda.is_available() else device
 
-        self.model = GCNConv(data.num_features, data.num_features, cached=True)
-        if device == 'cuda' and torch.cuda.is_available():
-            self.model = self.model.cuda()
+        self.model = GCNConv(data.num_features, data.num_features, cached=True).to(device)
         self.model.weight.data.copy_(torch.eye(data.num_features))  # identity transformation
         self.gc = self.model(data.x_raw, data.edge_index)
 
-    def run(self, logger):
-        # calculate error
+    def run(self):
         gc_hat = self.model(self.data.x, self.data.edge_index)
-        diff = (self.gc - gc_hat) / self.delta
-        diff[:, (self.delta == 0)] = 0  # avoid division by zero
+        diff = (self.gc - gc_hat) / self.sensitivity
         errors = torch.norm(diff, p=1, dim=1) / self.data.num_features
-
-        # obtain node degrees
-        row, col = self.data.edge_index
-        degrees = degree(row, self.data.num_nodes)
+        degrees = degree(self.data.edge_index[0], self.data.num_nodes)
 
         df = pd.DataFrame({'degree': degrees.cpu(), 'error': errors.cpu()})
         df = df[df['degree'] < df['degree'].quantile(q=self.max_degree_quantile)]
         values = df.groupby('degree').agg(['mean', 'std']).fillna(0).reset_index().values
 
         for deg, mae, std in values:
-            logger.log_metrics(metrics={'mae': mae, 'std': std}, step=deg)
+            self.logger.log_metrics(metrics={'mae': mae, 'std': std}, step=deg)
 
 
-def error_estimation(dataset, method, eps, repeats, save_dir, device):
+def error_estimation(dataset, method, eps, repeats, output_dir, device):
     for run in range(repeats):
         params = {
             'task': 'error',
@@ -57,25 +54,26 @@ def error_estimation(dataset, method, eps, repeats, save_dir, device):
         params_str = ' | '.join([f'{key}={val}' for key, val in params.items()])
         print(TermColors.FG.green + params_str + TermColors.reset)
 
-        experiment_name = f'error_{dataset.name}_{method}_{eps}'
-        logger = TensorBoardLogger(save_dir=save_dir, name=experiment_name, version=run)
+        output_dir = os.path.join(output_dir, 'error', dataset.name, method, str(eps))
+        logger = TensorBoardLogger(save_dir=output_dir, name=None)
 
-        data = Privatize(method=method, eps=eps)(dataset.get_data())
-        ErrorEstimation(data=data, device=device).run(logger)
+        privatize = Privatize(method=method, eps=eps)
+        dataset.add_transform(privatize)
+        ErrorEstimation(data=dataset[0], logger=logger, device=device).run()
 
 
 @torch.no_grad()
 def batch_error_estimation(args):
     for dataset_name in args.datasets:
-        dataset = GraphDataset(dataset_name=dataset_name, device=args.device)
+        dataset = GraphDataModule(name=dataset_name, normalize=(0, 1), device=args.device)
         for method in args.methods:
-            for eps in args.eps_list:
+            for eps in args.epsilons:
                 error_estimation(
                     dataset=dataset,
                     method=method,
                     eps=eps,
                     repeats=args.repeats,
-                    save_dir=args.output_dir,
+                    output_dir=args.output_dir,
                     device=args.device
                 )
 
@@ -87,13 +85,17 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('-d', '--datasets', nargs='+', choices=get_available_datasets(), required=True)
     parser.add_argument('-m', '--methods',  nargs='+', choices=get_available_mechanisms(), required=True)
-    parser.add_argument('-e', '--eps',      nargs='+', type=float, dest='eps_list', required=True)
+    parser.add_argument('-e', '--eps',      nargs='+', type=float, dest='epsilons', required=True)
     parser.add_argument('-r', '--repeats',      type=int, default=1)
     parser.add_argument('-o', '--output-dir',   type=str, default='./results')
     parser.add_argument('--device',             type=str, default='cuda', choices=['cpu', 'cuda'])
     args = parser.parse_args()
-    print(args)
 
+    # check if eps > 0 for LDP methods
+    if min(args.epsilons) <= 0:
+        parser.error('LDP methods require eps > 0.')
+
+    print(args)
     batch_error_estimation(args)
 
 
