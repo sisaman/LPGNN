c932aaa4ab02dea7dc2e1627d510377bc3a0e3eb
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-09-08 15:29:09 +0200

    add hops and aggr

--- error.py
+++ error.py
@@ -1,5 +1,6 @@
 import os
 from argparse import ArgumentParser
+from itertools import product
 
 import torch
 from pytorch_lightning import seed_everything
@@ -8,13 +9,14 @@ from torch_geometric.nn import GCNConv
 from torch_geometric.utils import degree
 import pandas as pd
 from datasets import get_available_datasets, GraphDataModule
+from models import KOrderPass
 from transforms import Privatize
 from utils import TermColors
 from privacy import get_available_mechanisms
 
 
 class ErrorEstimation:
-    def __init__(self, data, logger, max_degree_quantile=0.99, device='cuda'):
+    def __init__(self, data, k, agg, logger, max_degree_quantile=0.99, device='cuda'):
         self.data = data
         alpha = data.x_raw.min(dim=0)[0]
         beta = data.x_raw.max(dim=0)[0]
@@ -23,8 +25,7 @@ class ErrorEstimation:
         self.max_degree_quantile = max_degree_quantile
         device = 'cpu' if not torch.cuda.is_available() else device
 
-        self.model = GCNConv(data.num_features, data.num_features, cached=True).to(device)
-        self.model.weight.data.copy_(torch.eye(data.num_features))  # identity transformation
+        self.model = KOrderPass(K=k, aggr=agg).to(device)
         self.gc = self.model(data.x_raw, data.edge_index)
 
     def run(self):
@@ -41,41 +42,45 @@ class ErrorEstimation:
             self.logger.log_metrics(metrics={'mae': mae, 'std': std}, step=deg)
 
 
-def error_estimation(dataset, method, eps, repeats, output_dir, device):
+def error_estimation(dataset, method, eps, k, agg, repeats, output_dir, device):
     for run in range(repeats):
         params = {
             'task': 'error',
             'dataset': dataset.name,
             'method': method,
             'eps': eps,
+            'k': k,
+            'agg': agg,
             'run': run
         }
 
         params_str = ' | '.join([f'{key}={val}' for key, val in params.items()])
         print(TermColors.FG.green + params_str + TermColors.reset)
 
-        output_dir = os.path.join(output_dir, 'error', dataset.name, method, str(eps))
+        output_dir = os.path.join(output_dir, 'error', dataset.name, method, str(eps), str(k), agg)
         logger = TensorBoardLogger(save_dir=output_dir, name=None)
 
         privatize = Privatize(method=method, eps=eps)
         dataset.add_transform(privatize)
-        ErrorEstimation(data=dataset[0], logger=logger, device=device).run()
+        ErrorEstimation(data=dataset[0], k=k, agg=agg, logger=logger, device=device).run()
 
 
 @torch.no_grad()
 def batch_error_estimation(args):
     for dataset_name in args.datasets:
         dataset = GraphDataModule(name=dataset_name, normalize=(0, 1), device=args.device)
-        for method in args.methods:
-            for eps in args.epsilons:
-                error_estimation(
-                    dataset=dataset,
-                    method=method,
-                    eps=eps,
-                    repeats=args.repeats,
-                    output_dir=args.output_dir,
-                    device=args.device
-                )
+        configs = product(args.methods, args.epsilons, args.hops, args.aggs)
+        for method, eps, k, agg in configs:
+            error_estimation(
+                dataset=dataset,
+                method=method,
+                eps=eps,
+                k=k,
+                agg=agg,
+                repeats=args.repeats,
+                output_dir=args.output_dir,
+                device=args.device
+            )
 
 
 def main():
@@ -83,9 +88,11 @@ def main():
 
     # parse arguments
     parser = ArgumentParser()
-    parser.add_argument('-d', '--datasets', nargs='+', choices=get_available_datasets(), required=True)
-    parser.add_argument('-m', '--methods',  nargs='+', choices=get_available_mechanisms(), required=True)
-    parser.add_argument('-e', '--eps',      nargs='+', type=float, dest='epsilons', required=True)
+    parser.add_argument('-d', '--datasets',     nargs='+', choices=get_available_datasets(), required=True)
+    parser.add_argument('-m', '--methods',      nargs='+', choices=get_available_mechanisms(), required=True)
+    parser.add_argument('-e', '--eps',          nargs='+', type=float, dest='epsilons', required=True)
+    parser.add_argument('-k', '--hops',         nargs='*', type=int, default=[1])
+    parser.add_argument('-a', '--aggs',         nargs='*', type=str, default=['gcn'])
     parser.add_argument('-r', '--repeats',      type=int, default=1)
     parser.add_argument('-o', '--output-dir',   type=str, default='./results')
     parser.add_argument('--device',             type=str, default='cuda', choices=['cpu', 'cuda'])
