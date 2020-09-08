103b1fa89f0472d12a12bbe175c26e1276225079
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-09-08 17:48:16 +0200

    Update experiments.sh

--- experiments.sh
+++ experiments.sh
@@ -1,24 +1,21 @@
 # Error estimation
-python error.py -d cora elliptic facebook github mit twitch -m gm mbm -e 0.25 0.5 1 2 4 8 -k 1 2 5 10 20 50 100 200 -a gcn mean
+python error.py -d cora elliptic facebook github twitch -m gm mbm -e 0.5 1 2 4 -k 1 2 4 8 -a gcn mean
 
 # Node Classification
 ### Cora
-python train.py -d cora -m raw --lr 0.01 --weight-decay 0.01 -r 10
-
-### MIT
-python train.py -d mit -m raw --lr 0.01 --dropout 0.5  -r 10
+python train.py -d cora -m raw mbm gm -e 0.5 1 2 4 -k 1 2 4 8 --aggs gcn mean --lr 0.01 --weight-decay 0.01 -r 5
 
 ### Twitch
-python train.py -d twitch -m raw --lr 0.001 --weight-decay 0.01 --dropout 0.5 -r 10
+python train.py -d twitch -m raw mbm gm -e 0.5 1 2 4 -k 1 2 4 8 --aggs gcn mean --lr 0.001 --weight-decay 0.01 --dropout 0.5 -r 5
 
 ### Facebook
-python train.py -d facebook -m raw --lr 0.01 --weight-decay 0.001 --dropout 0.5 -r 10
+python train.py -d facebook -m raw mbm gm -e 0.5 1 2 4 -k 1 2 4 8 --aggs gcn mean --lr 0.01 --weight-decay 0.001 --dropout 0.5 -r 5
 
 ### Github
-python train.py -d github -m raw --lr 0.01 --dropout 0.5 -r 10
+python train.py -d github -m raw mbm gm -e 0.5 1 2 4 -k 1 2 4 8 --aggs gcn mean --lr 0.01 --dropout 0.5 -r 5
 
 ### Elliptic
-python train.py -d elliptic -m raw --lr 0.01 --dropout 0.5 -r 10
+python train.py -d elliptic -m raw mbm gm -e 0.5 1 2 4 -k 1 2 4 8 --aggs gcn mean --lr 0.01 --dropout 0.5 -r 5
 
 
 
