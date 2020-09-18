5d8eafa8f150dac2b1e417f644213324f42ac9ba
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-09-18 21:17:21 +0200

    full experiments

--- experiments.sh
+++ experiments.sh
@@ -1,23 +1,55 @@
-# Error estimation
-#python error.py -d cora elliptic facebook github twitch -m gm mbm -e 0.5 1 2 4 -k 1 2 4 8 -a gcn mean
+#################################################################################################
+###################################### ERROR ESTIMATION #########################################
+#################################################################################################
 
-# Node Classification
-### Cora
-python train.py -d cora -m raw mbm gm -e 0.5 1 2 4 -k 1 2 4 8 --aggs gcn mean --lr 0.01 --weight-decay 0.01 -r 10
-
-### Twitch
-python train.py -d twitch -m raw mbm gm -e 0.5 1 2 4 -k 1 2 4 8 --aggs gcn mean --lr 0.001 --weight-decay 0.01 --dropout 0.5 -r 10
-
-### Facebook
-python train.py -d facebook -m raw mbm gm -e 0.5 1 2 4 -k 1 2 4 8 --aggs gcn mean --lr 0.01 --weight-decay 0.001 --dropout 0.5 -r 10
-
-### Github
-python train.py -d github -m raw mbm gm -e 0.5 1 2 4 -k 1 2 4 8 --aggs gcn mean --lr 0.01 --dropout 0.5 -r 10
-
-### Elliptic
-python train.py -d elliptic -m raw mbm gm -e 0.5 1 2 4 -k 1 2 4 8 --aggs gcn mean --lr 0.01 --dropout 0.5 -r 10
+python error.py -d cora elliptic facebook github twitch -m gm mbm -e 0.5 1 2 4 -k 1 2 4 8 -a mean
 
 
 
+#################################################################################################
+##################################### NODE CLASSIFICATION #######################################
+#################################################################################################
+
+################### CORA ####################
+### DPGNN
+python train.py -d cora -m mbm -e 0.5 1 2 4 -k 1 2 4 8 16 32 64 -a mean --lr 0.01 --wd 0.01 --dp 0 -r 10
+### GCN+GM
+python train.py -d cora -m gm -e 0.5 1 2 4 -k 1 -a gcn --lr 0.01 --wd 0.01 --dp 0 -r 10
+### GCN
+python train.py -d cora -m raw -k 1 2 4 8 16 32 64 -a gcn --lr 0.01 --wd 0.01 --dp 0 -r 10
 
 
+################# ELLIPTIC ##################
+### DPGNN
+python train.py -d elliptic -m mbm -e 0.5 1 2 4 -k 1 2 4 8 16 32 64 -a mean --lr 0.01 --wd 0 --dp 0.5 -r 10
+### GCN+GM
+python train.py -d elliptic -m gm -e 0.5 1 2 4 -k 1 -a gcn --lr 0.01 --wd 0 --dp 0.5 -r 10
+### GCN
+python train.py -d elliptic -m raw -k 1 2 4 8 16 32 64 -a gcn --lr 0.01 --wd 0 --dp 0.5 -r 10
+
+
+################# FACEBOOK ##################
+### DPGNN
+python train.py -d facebook -m mbm -e 0.5 1 2 4 -k 1 2 4 8 16 32 64 -a mean --lr 0.01 --wd 0.001 --dp 0.5 -r 10
+### GCN+GM
+python train.py -d facebook -m gm -e 0.5 1 2 4 -k 1 -a gcn --lr 0.01 --wd 0.001 --dp 0.5 -r 10
+### GCN
+python train.py -d facebook -m raw -k 1 2 4 8 16 32 64 -a gcn --lr 0.01 --wd 0.001 --dp 0.5 -r 10
+
+
+################## GITHUB ###################
+### DPGNN
+python train.py -d github -m mbm -e 0.5 1 2 4 -k 1 2 4 8 16 32 64 -a mean --lr 0.01 --wd 0 --dp 0.5 -r 10
+### GCN+GM
+python train.py -d github -m gm -e 0.5 1 2 4 -k 1 -a gcn --lr 0.01 --wd 0 --dp 0.5 -r 10
+### GCN
+python train.py -d github -m raw -k 1 2 4 8 16 32 64 -a gcn --lr 0.01 --wd 0 --dp 0.5 -r 10
+
+
+################## TWITCH ###################
+### DPGNN
+python train.py -d twitch -m mbm -e 0.5 1 2 4 -k 1 2 4 8 16 32 64 -a mean --lr 0.001 --wd 0.01 --dp 0.5 -r 10
+### GCN+GM
+python train.py -d twitch -m gm -e 0.5 1 2 4 -k 1 -a gcn --lr 0.001 --wd 0.01 --dp 0.5 -r 10
+### GCN
+python train.py -d twitch -m raw -k 1 2 4 8 16 32 64 -a gcn --lr 0.001 --wd 0.01 --dp 0.5 -r 10
