641899928bd77d9a1d3fe3405659484d32e0429d
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-09-29 20:12:08 +0200

    bring back piecewise mechanism

--- privacy.py
+++ privacy.py
@@ -129,9 +129,55 @@ class MultiBit(Mechanism):
         return x_prime
 
 
+class Piecewise(Mechanism):
+    def transform(self, x):
+        # normalize x between -1,1
+        t = (x - self.alpha) / (self.beta - self.alpha)
+        t = 2 * t - 1
+
+        # piecewise mechanism's variables
+        P = (math.exp(self.eps) - math.exp(self.eps / 2)) / (2 * math.exp(self.eps / 2) + 2)
+        C = (math.exp(self.eps / 2) + 1) / (math.exp(self.eps / 2) - 1)
+        L = t * (C + 1) / 2 - (C - 1) / 2
+        R = L + C - 1
+
+        # thresholds for random sampling
+        threshold_left = P * (L + C) / math.exp(self.eps)
+        threshold_right = threshold_left + P * (R - L)
+
+        # masks for piecewise random sampling
+        x = torch.rand_like(t)
+        mask_left = x < threshold_left
+        mask_middle = (threshold_left < x) & (x < threshold_right)
+        mask_right = threshold_right < x
+
+        # random sampling
+        t = mask_left * (torch.rand_like(t) * (L + C) - C)
+        t += mask_middle * (torch.rand_like(t) * (R - L) + L)
+        t += mask_right * (torch.rand_like(t) * (C - R) + R)
+
+        # unbias data
+        y_star = (self.beta - self.alpha) * (t + 1) / 2 + self.alpha
+        return y_star
+
+
+class MultiDimPiecewise(Piecewise):
+    def transform(self, x):
+        n, d = x.size()
+        k = int(max(1, min(d, math.floor(self.eps / 2.5))))
+        sample = torch.cat([torch.randperm(d, device=x.device)[:k] for _ in range(n)]).view(n, k)
+        mask = torch.zeros_like(x, dtype=torch.bool)
+        mask.scatter_(dim=1, index=sample, value=True)
+        self.eps /= k
+        y = super().transform(x)
+        z = mask * y * d / k
+        return z
+
+
 _available_mechanisms = {
-    'gm': Gaussian,
+    'agm': Gaussian,
     'mbm': MultiBit,
+    'pwm': MultiDimPiecewise
 }
 
 
