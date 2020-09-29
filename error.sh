100c866245e9f0df95d348a0e56a254d978b7926
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-09-30 00:42:34 +0200

    shell files

---
+++ error.sh
@@ -0,0 +1,2 @@
+python error.py -t eps -d cora citeseer pubmed facebook github lastfm -m agm cgm lpm obm mbm pwm -e 0.5 1 2 4 -a mean gcn
+python error.py -t deg -d cora citeseer pubmed facebook github lastfm -m agm cgm lpm obm mbm pwm -e 1 -a mean gcn
