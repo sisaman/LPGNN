fa754c571481cf06af3f9dac69b54659f40d59e6
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-09-30 00:19:24 +0200

    fix Gaussian mechanims bug; add OneBit, Laplace, and Classic Gaussian mechanism

--- privacy.py
+++ privacy.py
@@ -38,10 +38,44 @@ class Gaussian(Mechanism):
             d = x.size(1)
             self.sensitivity = len_interval * math.sqrt(d)
 
-        self.sigma = self.calibrate_analytic_gaussian_mechanism()
+        self.sigma = self.calibrate_gaussian_mechanism()
 
     def transform(self, x):
-        return torch.normal(mean=x, std=self.sensitivity)
+        return torch.normal(mean=x, std=self.sigma)
+
+    def calibrate_gaussian_mechanism(self):
+        return self.sensitivity * math.sqrt(2 * math.log(1.25 / self.delta)) / self.eps
+
+
+class AnalyticGaussian(Gaussian):
+    def calibrate_gaussian_mechanism(self, tol=1.e-12):
+        """ Calibrate a Gaussian perturbation for differential privacy
+        using the analytic Gaussian mechanism of [Balle and Wang, ICML'18]
+        Arguments:
+        tol : error tolerance for binary search (tol > 0)
+        Output:
+        sigma : standard deviation of Gaussian noise needed to achieve (epsilon,delta)-DP under global sensitivity GS
+        """
+        delta_thr = self._case_a(0.0)
+        if self.delta == delta_thr:
+            alpha = 1.0
+        else:
+            if self.delta > delta_thr:
+                predicate_stop_DT = lambda s: self._case_a(s) >= self.delta
+                function_s_to_delta = lambda s: self._case_a(s)
+                predicate_left_BS = lambda s: function_s_to_delta(s) > self.delta
+                function_s_to_alpha = lambda s: math.sqrt(1.0 + s / 2.0) - math.sqrt(s / 2.0)
+            else:
+                predicate_stop_DT = lambda s: self._case_b(s) <= self.delta
+                function_s_to_delta = lambda s: self._case_b(s)
+                predicate_left_BS = lambda s: function_s_to_delta(s) < self.delta
+                function_s_to_alpha = lambda s: math.sqrt(1.0 + s / 2.0) + math.sqrt(s / 2.0)
+            predicate_stop_BS = lambda s: abs(function_s_to_delta(s) - self.delta) <= tol
+            s_inf, s_sup = self._doubling_trick(predicate_stop_DT, 0.0, 1.0)
+            s_final = self._binary_search(predicate_stop_BS, predicate_left_BS, s_inf, s_sup)
+            alpha = function_s_to_alpha(s_final)
+        sigma = alpha * self.sensitivity / math.sqrt(2.0 * self.eps)
+        return sigma
 
     @staticmethod
     def _phi(t):
@@ -71,44 +105,20 @@ class Gaussian(Mechanism):
             s_mid = s_inf + (s_sup - s_inf) / 2.0
         return s_mid
 
-    def calibrate_analytic_gaussian_mechanism(self, tol=1.e-12):
-        """ Calibrate a Gaussian perturbation for differential privacy
-        using the analytic Gaussian mechanism of [Balle and Wang, ICML'18]
-        Arguments:
-        tol : error tolerance for binary search (tol > 0)
-        Output:
-        sigma : standard deviation of Gaussian noise needed to achieve (epsilon,delta)-DP under global sensitivity GS
-        """
-        delta_thr = self._case_a(0.0)
-        if self.delta == delta_thr:
-            alpha = 1.0
-        else:
-            if self.delta > delta_thr:
-                predicate_stop_DT = lambda s: self._case_a(s) >= self.delta
-                function_s_to_delta = lambda s: self._case_a(s)
-                predicate_left_BS = lambda s: function_s_to_delta(s) > self.delta
-                function_s_to_alpha = lambda s: math.sqrt(1.0 + s / 2.0) - math.sqrt(s / 2.0)
-            else:
-                predicate_stop_DT = lambda s: self._case_b(s) <= self.delta
-                function_s_to_delta = lambda s: self._case_b(s)
-                predicate_left_BS = lambda s: function_s_to_delta(s) < self.delta
-                function_s_to_alpha = lambda s: math.sqrt(1.0 + s / 2.0) + math.sqrt(s / 2.0)
-            predicate_stop_BS = lambda s: abs(function_s_to_delta(s) - self.delta) <= tol
-            s_inf, s_sup = self._doubling_trick(predicate_stop_DT, 0.0, 1.0)
-            s_final = self._binary_search(predicate_stop_BS, predicate_left_BS, s_inf, s_sup)
-            alpha = function_s_to_alpha(s_final)
-        sigma = alpha * self.sensitivity / math.sqrt(2.0 * self.eps)
-        return sigma
-
 
 class MultiBit(Mechanism):
-    def __init__(self, *args, m=None, **kwargs):
+    def __init__(self, *args, m='best', **kwargs):
         super().__init__(*args, **kwargs)
         self.m = m
 
     def transform(self, x):
         n, d = x.size()
-        m = int(max(1, min(d, math.floor(self.eps / 2.18)))) if self.m is None else self.m
+        if self.m == 'best':
+            m = int(max(1, min(d, math.floor(self.eps / 2.18))))
+        elif self.m == 'max':
+            m = d
+        else:
+            m = self.m
 
         # sample features for perturbation
         bigS = torch.cat([torch.randperm(d, device=x.device)[:m] for _ in range(n)]).view(n, m)
@@ -174,10 +184,27 @@ class MultiDimPiecewise(Piecewise):
         return z
 
 
+class OneBit(MultiBit):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, m='max', **kwargs)
+
+
+class Laplace(Mechanism):
+    def transform(self, x):
+        d = x.size(1)
+        sensitivity = (self.beta - self.alpha) * d
+        scale = torch.ones_like(x) * (sensitivity / self.eps)
+        y_star = torch.distributions.Laplace(x, scale).sample()
+        return y_star
+
+
 _available_mechanisms = {
-    'agm': Gaussian,
+    'agm': AnalyticGaussian,
     'mbm': MultiBit,
-    'pwm': MultiDimPiecewise
+    'pwm': MultiDimPiecewise,
+    'obm': OneBit,
+    'lpm': Laplace,
+    'cgm': Gaussian
 }
 
 
