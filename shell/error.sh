9648337c2601ec3d6b28759b5e82c61bbabe8f8f
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2020-10-01 15:23:05 +0200

    update shell

--- shell/error.sh
+++ shell/error.sh
@@ -1,3 +1,3 @@
 cd ..
-python src/error.py -t eps -d cora citeseer pubmed facebook github lastfm -m agm obm mbm -e 0.5 1 2 4 -a mean gcn
-python src/error.py -t deg -d cora citeseer pubmed facebook github lastfm -m agm obm mbm -e 1 -a mean gcn
+python error.py -t eps -d cora citeseer pubmed facebook github lastfm -m agm obm mbm -e 0.5 1 2 4 -a mean gcn
+python error.py -t deg -d cora citeseer pubmed facebook github lastfm -m agm obm mbm -e 1 -a mean gcn
--- shell/train.sh
+++ shell/train.sh
@@ -2,8 +2,8 @@ REP=10
 DIR="./temp"
 
 cd ..
-python src/train.py -d "$DB" -m mbm -e 1 -k 1 2 4 8 16 32 --lr "$LR" --wd "$WD" --dp "$DP" -r $REP -o $DIR
-python src/train.py -d "$DB" -m mbm -e 0.1 0.5 1 2 4 -k 1 2 4 8 16 32 --no-loops --lr "$LR" --wd "$WD" --dp "$DP" -r $REP -o $DIR
-python src/train.py -d "$DB" -m raw -k 1 --lr "$LR" --wd "$WD" --dp "$DP" -r $REP -o $DIR
-python src/train.py -d "$DB" -m raw -k 1 2 4 8 16 32 --no-loops --lr "$LR" --wd "$WD" --dp "$DP" -r $REP -o $DIR
-python src/train.py -d "$DB" -m rnd -k 1 --lr "$LR" --wd "$WD" --dp "$DP" -r $REP -o $DIR
+python train.py -d "$DB" -m mbm -e 1 -k 1 2 4 8 16 32 --lr "$LR" --wd "$WD" --dp "$DP" -r $REP -o $DIR
+python train.py -d "$DB" -m mbm -e 0.1 0.5 1 2 4 -k 1 2 4 8 16 32 --no-loops --lr "$LR" --wd "$WD" --dp "$DP" -r $REP -o $DIR
+python train.py -d "$DB" -m raw -k 1 --lr "$LR" --wd "$WD" --dp "$DP" -r $REP -o $DIR
+python train.py -d "$DB" -m raw -k 1 2 4 8 16 32 --no-loops --lr "$LR" --wd "$WD" --dp "$DP" -r $REP -o $DIR
+python train.py -d "$DB" -m rnd -k 1 --lr "$LR" --wd "$WD" --dp "$DP" -r $REP -o $DIR
