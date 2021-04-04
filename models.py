940ce72f728f64812f6ee9e0757f6ec067c35a47
sajadmanesh <sina.sajadmanesh@gmail.com>
2021-04-04 18:04:55 +0200

    temp commit

--- models.py
+++ models.py
@@ -26,6 +26,9 @@ class KProp(MessagePassing):
         return x
 
     def neighborhood_aggregation(self, x, adj_t):
+        if self.K <= 0:
+            return x
+
         if self.normalize:
             adj_t = gcn_norm(adj_t, add_self_loops=False)
 
@@ -63,20 +66,6 @@ class GNN(torch.nn.Module):
         return x
 
 
-class LabelGNN(torch.nn.Module):
-    def __init__(self, num_classes, steps, aggregator):
-        super().__init__()
-        self.conv1 = KProp(
-            in_channels=num_classes, out_channels=num_classes, steps=steps,
-            aggregator=aggregator, add_self_loops=False, normalize=True, cached=False
-        )
-
-    def forward(self, y, adj_t):
-        y = self.conv1.neighborhood_aggregation(y, adj_t)
-        y = F.log_softmax(y, dim=1)
-        return y
-
-
 class NodeClassifier(torch.nn.Module):
     def __init__(self,
                  input_dim,
@@ -85,63 +74,53 @@ class NodeClassifier(torch.nn.Module):
                  dropout: dict(help='dropout rate (between zero and one)') = 0.0,
                  x_steps: dict(help='KProp step parameter', option='-k') = 1,
                  y_steps: dict(help='number of label propagation steps') = 0,
-                 aggregator: dict(help='GNN aggregator function', choices=['add', 'mean']) = 'add',
                  batch_norm: dict(help='use batch-normalization') = True,
                  add_self_loops: dict(help='whether to add self-loops to the graph') = True,
                  ):
         super().__init__()
 
-        self.y_steps = y_steps
-
-        self.x_gnn = GNN(
+        self.gnn = GNN(
             input_dim=input_dim, output_dim=num_classes, hidden_dim=hidden_dim, steps=x_steps,
-            aggregator=aggregator, batch_norm=batch_norm, dropout=dropout, add_self_loops=add_self_loops
+            aggregator='add', batch_norm=batch_norm, dropout=dropout, add_self_loops=add_self_loops
         )
-        self.y_gnn = LabelGNN(num_classes=num_classes, steps=y_steps, aggregator=aggregator)
+        self.label_prop = KProp(
+            1, 1, steps=y_steps, aggregator='add', add_self_loops=add_self_loops, normalize=True, cached=False
+        ).neighborhood_aggregation
 
     def forward(self, data):
-        return self.x_gnn(data)
+        return self.gnn(data)
 
-    def evaluate(self, data, mask):
+    def step(self, data, mask):
+        p_y_x = self(data)
+        p_yp_x = torch.matmul(p_y_x, data.T)
+        p_yt_x = torch.log(self.label_prop(p_yp_x, data.adj_t))
 
-        if self.y_steps == 0:
-            out = self(data)[mask]
-            target = data.y[mask]
-        else:
-            p_y_x = self(data)
-            p_yp_x = torch.matmul(p_y_x, data.p)
-            p_yt_x = self.y_gnn(p_yp_x, data.adj_t)
+        yt = data.y.clone()
+        yt[data.test_mask] = 0
+        p_yt_yp = torch.softmax(self.label_prop(yt, data.adj_t), dim=1)  # todo might not work for non-priv labels
 
-            yp = data.y.clone()
-            yp_test = torch.randint(low=0, high=data.num_classes, size=(data.test_mask.sum(), 1), device=data.y.device)
-            yp[data.test_mask] = 0
-            yp[data.test_mask].scatter_(1, yp_test, 1)
-            p_yt_yp = self.y_gnn(yp, data.adj_t)
+        out = p_yt_x[mask]
+        target = p_yt_yp[mask].argmax(dim=1)
 
-            out = p_yt_x[mask]
-            target = p_yt_yp[mask]
-
-        target = target.argmax(dim=1)
         loss = F.nll_loss(input=out, target=target)
         acc = accuracy(pred=out.argmax(dim=1), target=target) * 100
         return loss, acc
 
     def training_step(self, data):
         mask = data.train_mask
-        loss, acc = self.evaluate(data, mask=mask)
+        loss, acc = self.step(data, mask=mask)
         metrics = {'train_loss': loss.item(), 'train_acc': acc}
         return loss, metrics
 
     def validation_step(self, data):
         mask = data.val_mask
-        loss, acc = self.evaluate(data, mask=mask)
+        loss, acc = self.step(data, mask=mask)
         metrics = {'val_loss': loss.item(), 'val_acc': acc}
         metrics.update(self.test_step(data))
         return metrics
 
     def test_step(self, data):
-        out = self(data)[data.test_mask]
-        pred = out.argmax(dim=1)
+        pred = self(data)[data.test_mask].argmax(dim=1)
         target = data.y[data.test_mask].argmax(dim=1)
         acc = accuracy(pred=pred, target=target) * 100
         return {'test_acc': acc}
