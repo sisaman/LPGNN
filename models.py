71a3135c32b7c060904c6d593f4c89477de967c3
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2021-04-08 17:17:55 +0200

    structure code

--- models.py
+++ models.py
@@ -5,25 +5,26 @@ from torch_geometric.utils import accuracy
 from torch.nn import Linear, Dropout
 from torch_geometric.nn import MessagePassing, BatchNorm
 from torch_sparse import matmul
+eps = 1e-20
 
 
 class KProp(MessagePassing):
-    def __init__(self, in_channels, out_channels, steps, aggregator, add_self_loops, normalize, cached=False):
+    def __init__(self, steps, aggregator, add_self_loops, normalize, cached):
         super().__init__(aggr=aggregator)
-        self.fc = Linear(in_channels, out_channels)
         self.K = steps
         self.add_self_loops = add_self_loops
         self.normalize = normalize
         self.cached = cached
         self._cached_x = None
 
+    def reset_parameters(self):
+        self._cached_x = None
+
     def forward(self, x, adj_t):
         if self._cached_x is None or not self.cached:
-            x = self.neighborhood_aggregation(x, adj_t)
-            self._cached_x = x
+            self._cached_x = self.neighborhood_aggregation(x, adj_t)
 
-        x = self.fc(self._cached_x)
-        return x
+        return self._cached_x
 
     def neighborhood_aggregation(self, x, adj_t):
         if self.K <= 0:
@@ -44,26 +45,58 @@ class KProp(MessagePassing):
         return matmul(adj_t, x, reduce=self.aggr)
 
 
+class KPropConv(KProp):
+    def __init__(self, in_channels, out_channels, steps, aggregator, add_self_loops, normalize, cached):
+        super().__init__(steps, aggregator, add_self_loops, normalize, cached=cached)
+        self.fc = Linear(in_channels, out_channels)
+
+    def forward(self, x, adj_t):
+        x = super().forward(x, adj_t)
+        x = self.fc(x)
+        return x
+
+    def reset_parameters(self):
+        super().reset_parameters()
+        self.fc.reset_parameters()
+
+
 class GNN(torch.nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim, steps, aggregator, batch_norm, dropout, add_self_loops):
         super().__init__()
-        self.conv1 = KProp(input_dim, hidden_dim, steps=steps, aggregator=aggregator,
-                           add_self_loops=add_self_loops, normalize=True, cached=True)
-        self.conv2 = KProp(hidden_dim, output_dim, steps=1, aggregator=aggregator,
-                           add_self_loops=True, normalize=True, cached=False)
+        self.conv1 = KPropConv(input_dim, hidden_dim, steps=steps, aggregator=aggregator,
+                               add_self_loops=add_self_loops, normalize=True, cached=True)
+        self.conv2 = KPropConv(hidden_dim, output_dim, steps=1, aggregator=aggregator,
+                               add_self_loops=True, normalize=True, cached=False)
         self.bn = BatchNorm(hidden_dim) if batch_norm else None
         self.dropout = Dropout(p=dropout)
 
     def forward(self, data):
         x, adj_t = data.x, data.adj_t
         x = self.conv1(x, adj_t)
-        if self.bn:
-            x = self.bn(x)
+        x = self.bn(x) if self.bn else x
         x = torch.selu(x)
         x = self.dropout(x)
         x = self.conv2(x, adj_t)
         x = F.softmax(x, dim=1)
         return x
+
+    def reset_parameters(self):
+        for layer in self.children():
+            layer.reset_parameters()
+
+
+class LabelGNN(torch.nn.Module):
+    def __init__(self, y_steps):
+        super().__init__()
+        self.kprop = KProp(steps=y_steps, aggregator='add', add_self_loops=False, normalize=True, cached=False)
+
+    def forward(self, y, adj_t):
+        y = self.kprop(y, adj_t)
+        y = torch.softmax(y, dim=1)
+        return y
+
+    def reset_parameters(self):
+        self.kprop.reset_parameters()
 
 
 class NodeClassifier(torch.nn.Module):
@@ -74,7 +107,6 @@ class NodeClassifier(torch.nn.Module):
                  dropout: dict(help='dropout rate (between zero and one)') = 0.0,
                  x_steps: dict(help='KProp step parameter', option='-k') = 1,
                  y_steps: dict(help='number of label propagation steps') = 0,
-                 correct_loss: dict(help='perform loss correction') = True,
                  propagate_predictions: dict(help='whether to propagate predictions') = True,
                  batch_norm: dict(help='use batch-normalization') = True,
                  add_self_loops: dict(help='whether to add self-loops to the graph') = True,
@@ -82,50 +114,48 @@ class NodeClassifier(torch.nn.Module):
         super().__init__()
 
         self.propagate_predictions = propagate_predictions
-        self.correct_loss = correct_loss
         self.y_steps = y_steps
 
-        self.gnn = GNN(
+        self.x_gnn = GNN(
             input_dim=input_dim, output_dim=num_classes, hidden_dim=hidden_dim, steps=x_steps,
             aggregator='add', batch_norm=batch_norm, dropout=dropout, add_self_loops=add_self_loops
         )
-        self.prop = KProp(
-            1, 1, steps=y_steps, aggregator='add', add_self_loops=False, normalize=True, cached=False
-        ).neighborhood_aggregation
+
+        self.y_gnn = LabelGNN(y_steps=y_steps)
+
+    def reset_parameters(self):
+        self.x_gnn.reset_parameters()
+        self.y_gnn.reset_parameters()
 
     def forward(self, data):
-        return self.gnn(data)
+        return self.x_gnn(data)
 
-    def evaluate(self, data, mask):
-        p_y_x = self(data)  # P(y|x')
-        p_yp_x = torch.matmul(p_y_x, data.T) if self.correct_loss else p_y_x  # P(y'|x')
-        p_yt_x = self.prop(p_yp_x, data.adj_t) if self.propagate_predictions else p_yp_x  # P(y~|x')
+    def step(self, data, mask):
+        p_y_x = p_yt_x = self.x_gnn(data)  # P(y|x')
 
-        yt_yp = data.y.float()
-        yt_yp[data.test_mask] = 0  # to avoid using test labels
-        yt_yp = self.prop(yt_yp, data.adj_t)  # y~
+        if self.propagate_predictions:
+            p_yp_x = torch.matmul(p_y_x, data.T)  # P(y'|x')
+            p_yt_x = self.y_gnn(p_yp_x, data.adj_t)  # P(y~|x')
 
-        if self.y_steps > 0:
-            log_p_yt_x = torch.log_softmax(p_yt_x, dim=1)
-        else:
-            log_p_yt_x = torch.log(p_yt_x + 1e-10)
+        yp = data.y.float()
+        yp[data.test_mask] = 0  # to avoid using test labels
+        yt = self.y_gnn(yp, data.adj_t)  # y~
 
-        out = log_p_yt_x[mask]
-        target = yt_yp[mask].argmax(dim=1)
-
-        loss = F.nll_loss(input=out, target=target)
-        acc = accuracy(pred=out.argmax(dim=1), target=target) * 100
+        out = p_yt_x[mask]
+        target = yt[mask]
+        loss = F.nll_loss(input=torch.log(out + eps), target=target.argmax(dim=1))
+        acc = accuracy(pred=out.argmax(dim=1), target=target.argmax(dim=1)) * 100
         return loss, acc
 
     def training_step(self, data):
         mask = data.train_mask
-        loss, acc = self.evaluate(data, mask)
+        loss, acc = self.step(data, mask)
         metrics = {'train_loss': loss.item(), 'train_acc': acc}
         return loss, metrics
 
     def validation_step(self, data):
         mask = data.val_mask
-        loss, acc = self.evaluate(data, mask)
+        loss, acc = self.step(data, mask)
         metrics = {'val_loss': loss.item(), 'val_acc': acc}
         metrics.update(self.test_step(data))
         return metrics
