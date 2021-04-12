4a2e482da69361145a3447dfe5c6dd6cab803c64
Sina Sajadmanesh <sina.sajadmanesh@gmail.com>
2021-04-12 19:20:11 +0200

    Update experiments.py

--- experiments.py
+++ experiments.py
@@ -1,3 +1,4 @@
+import os.path
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from itertools import product
 
@@ -7,10 +8,11 @@ from utils import print_args, JobManager
 
 
 class HyperParams:
-    def __init__(self, path):
-        self.df = pd.read_pickle(path)
+    def __init__(self, path_dir):
+        self.df_params = pd.read_pickle(os.path.join(path_dir, 'params.pkl'))
+        self.df_steps = pd.read_pickle(os.path.join(path_dir, 'steps.pkl'))
 
-    def get(self, dataset, feature='raw', x_eps=np.inf, y_eps=np.inf):
+    def get(self, dataset, feature, x_eps, y_eps, return_steps):
         if feature == 'crnd':
             feature = 'rnd'
         if x_eps < np.inf:
@@ -18,26 +20,31 @@ class HyperParams:
         if y_eps < np.inf:
             y_eps = 1
 
-        return self.df.loc[
+        hparams = self.df_params.loc[
             feature, x_eps, y_eps, dataset
         ][['learning_rate', 'weight_decay', 'dropout']].to_dict()
 
+        if return_steps:
+            steps = self.df_steps.loc[dataset, x_eps, y_eps].to_dict()
+            if x_eps == np.inf: steps['x_steps'] = 0
+            if y_eps == np.inf: steps['y_steps'] = 0
+            hparams.update(steps)
 
-def generate_command(args, options):
-    default_args = f' -s {args.seed} -r {args.repeats} -o results --log --log-mode collective --project-name {args.project} '
-    command = f'python main.py {default_args} {options}'
-    return command
+        return hparams
 
 
-def get_option_string(dataset, feature, mechanism, model, x_eps, x_steps, y_eps, y_steps, learning_rate, weight_decay, dropout):
-    return f"--dataset {dataset} --feature {feature} --mechanism {mechanism} --model {model} " \
+def get_experiment_cmd(dataset, feature, mechanism, model, x_eps, x_steps, y_eps, y_steps, forward_correction,
+                       learning_rate, weight_decay, dropout, args):
+    return f"python main.py --dataset {dataset} --feature {feature} --mechanism {mechanism} --model {model} " \
            f"--x-eps {x_eps} --x-steps {x_steps} --y-eps {y_eps} --y-steps {y_steps} " \
-           f"--learning-rate {learning_rate} --weight-decay {weight_decay} --dropout {dropout}"
+           f"--forward-correction {forward_correction} --learning-rate {learning_rate} " \
+           f"--weight-decay {weight_decay} --dropout {dropout} -s {args.seed} -r {args.repeats} " \
+           f"-o {args.output_dir} --log --log-mode collective --project-name {args.project}"
 
 
 def experiment_commands(args):
     run_cmds = []
-    hparams = HyperParams(path='hparams/hparams.pkl')
+    hparams = HyperParams(path_dir='./hparams')
 
     ## LPGNN ALL CASES
 
@@ -47,13 +54,16 @@ def experiment_commands(args):
     y_eps_list = [1, 2, 3, 4, np.inf]
     y_steps_list = [0, 2, 4, 8, 16]
 
-    for dataset, x_eps, x_steps, y_eps, y_steps in product(datasets, x_eps_list, x_steps_list, y_eps_list, y_steps_list):
-        params = hparams.get(dataset=dataset, feature='raw', x_eps=x_eps, y_eps=y_eps)
-        options = get_option_string(dataset=dataset, feature='raw', mechanism='mbm', model='sage',
-                                    x_eps=x_eps, x_steps=x_steps, y_eps=y_eps, y_steps=y_steps, **params)
-        command = generate_command(args, options)
+    for dataset, x_eps, x_steps, y_eps, y_steps in product(datasets, x_eps_list, x_steps_list, y_eps_list,
+                                                           y_steps_list):
+        params = hparams.get(dataset=dataset, feature='raw', x_eps=x_eps, y_eps=y_eps, return_steps=False)
+        command = get_experiment_cmd(
+            dataset=dataset, feature='raw', mechanism='mbm', model='sage',
+            x_eps=x_eps, x_steps=x_steps, y_eps=y_eps, y_steps=y_steps,
+            forward_correction=True, learning_rate=params['learning_rate'],
+            weight_decay=params['weight_decay'], dropout=params['dropout'], args=args
+        )
         run_cmds.append(command)
-
 
     ## FULLY-PRIVATE BASELINES
 
@@ -61,12 +71,45 @@ def experiment_commands(args):
     features = ['rnd', 'crnd', 'one', 'ohd']
 
     for dataset, feature in product(datasets, features):
-        params = hparams.get(dataset=dataset, feature=feature)
-        options = get_option_string(dataset=dataset, feature=feature, mechanism='mbm', model='sage',
-                                    x_eps=np.inf, x_steps=0, y_eps=np.inf, y_steps=0, **params)
-        command = generate_command(args, options)
+        params = hparams.get(dataset=dataset, feature=feature, x_eps=np.inf, y_eps=np.inf, return_steps=False)
+        command = get_experiment_cmd(
+            dataset=dataset, feature=feature, mechanism='mbm', model='sage',
+            x_eps=np.inf, x_steps=0, y_eps=np.inf, y_steps=0, forward_correction=True,
+            learning_rate=params['learning_rate'], weight_decay=params['weight_decay'],
+            dropout=params['dropout'], args=args
+        )
         run_cmds.append(command)
 
+    ## BASELINE LDP MECHANISMS
+
+    datasets = ['cora', 'pubmed', 'facebook', 'lastfm']
+    mechanisms = ['1bm', 'lpm', 'agm']
+    x_eps_list = [0.01, 0.1, 1, 2, 3]
+
+    for dataset, mechanism, x_eps in product(datasets, mechanisms, x_eps_list):
+        params = hparams.get(dataset=dataset, feature='raw', x_eps=x_eps, y_eps=np.inf, return_steps=True)
+        command = get_experiment_cmd(
+            dataset=dataset, feature='raw', mechanism=mechanism, model='sage',
+            x_eps=x_eps, x_steps=params['x_steps'], y_eps=np.inf, y_steps=0,
+            forward_correction=True, learning_rate=params['learning_rate'],
+            weight_decay=params['weight_decay'], dropout=params['dropout'], args=args
+        )
+        run_cmds.append(command)
+
+    ## NO LABEL CORRECTION
+
+    datasets = ['cora', 'pubmed', 'facebook', 'lastfm']
+    y_eps_list = [1, 2, 3]
+
+    for dataset, y_eps in product(datasets, y_eps_list):
+        params = hparams.get(dataset=dataset, feature='raw', x_eps=np.inf, y_eps=y_eps, return_steps=False)
+        command = get_experiment_cmd(
+            dataset=dataset, feature='raw', mechanism='mbm', model='sage',
+            x_eps=np.inf, x_steps=0, y_eps=y_eps, y_steps=0,
+            forward_correction=False, learning_rate=params['learning_rate'],
+            weight_decay=params['weight_decay'], dropout=params['dropout'], args=args
+        )
+        run_cmds.append(command)
 
     run_cmds = list(set(run_cmds))
     return run_cmds
@@ -75,6 +118,7 @@ def experiment_commands(args):
 def main():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     parser, parser_create = JobManager.register_arguments(parser)
+    parser.add_argument('-o', '--output-dir', type=str, default='./results', help="directory to store the results")
     parser_create.add_argument('--project', type=str, default='LPGNN-experiments',
                                help='project name for wandb logging')
     parser_create.add_argument('-s', '--seed', type=int, default=12345, help='initial random seed')
